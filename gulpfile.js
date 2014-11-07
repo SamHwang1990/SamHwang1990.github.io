d8d77a5adb47812f8698b5aced162febf60e1727
SamHwang1990 <samhwang1990@gmail.com>
2014-11-07 17:56:32 +0800

    use webstorm compile function instead of gulp-compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,3 @@
 // 引入 gulp
-var gulp = require('gulp');
-
-// 引入 Plugins
-var compass = require('gulp-compass');
-
-// 创建 Compass 任务
-gulp.task('compass', function() {
-    gulp.src('./design/Stylesheet/scss/**')
-        .pipe(compass({
-            comments: false,
-            css: 'css',
-            sass: 'scss',
-            image: 'img'
-        }))
-        .pipe(gulp.dest('./design/Stylesheet/css'));
-});
-
-// 默认任务
-gulp.task('default', function() {
-    gulp.run('compass');
-
-    gulp.watch([
-        './design/Stylesheet/scss/**',
-        './design/Image/**'
-    ], function(event) {
-        gulp.run('compass');
-    });
-});
+var gulp = require('gulp'),
+path = require('path');
